7b2de0c28b9dceffce4a1f2b12cb47c837c69ec2
jesser <jesserslimi315@gmail.com>
2025-11-24 10:10:45 +0100

    finished the primary work the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       <div>
+
+        
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
