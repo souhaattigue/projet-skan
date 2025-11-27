b8de4e1949c319cf10a8ca3047e76f878f89d3e1
veryfastlearner <ahmed.benmim@supcom.tn>
2025-11-27 11:27:08 +0100

    routed 404 pag

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./Components/Header"
 import Footer from "./Components/Footer";
 import Ad from "./Components/Ad"
 import ProfilePage from "./Components/ProfilePage"
+import NotFound from "./Components/NotFound"
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+      {/* Components that appear on all pages */}
       <Ad />
       <Header />
-      <ProfilePage />
+
+      {/* Routes - Different pages based on URL */}
+      <Routes>
+        {/* Home page - ProfilePage */}
+        <Route path="/" element={<ProfilePage />} />
+
+        {/* 404 Not Found - Catch all unmatched routes */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      {/* Footer appears on all pages */}
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./NotFound.css"
 
-function NotFound(){
+function NotFound() {
     return (
         <div className="not-found-container">
             <h1 className="title">404 Not Found</h1>
-            <p>Your visited page is not found.You may go Home page </p>
-            <button className="Home">Back to home page</button>
+            <p>Your visited page is not found. You may go Home page</p>
+            <Link to="/">
+                <button className="Home">Back to home page</button>
+            </Link>
         </div>
-        
     )
 }
 export default NotFound;
